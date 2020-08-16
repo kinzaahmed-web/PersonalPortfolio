f83e6bbdd63337fae1c4e4546159da5fb8b97936
kinzaahmed-web <kinza1013@yahoo.com>
2020-08-15 22:45:05 -0500

    changes
    changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-$('.skill-per').each(function(){
-    var $this = $(this);
-    var per = $this.attr('per');
-    $this.css("width",per+'%');
-    $({animatedValue: 0}).animate({animatedValue: per},{
-        duration: 1000,
-        step: function(){
-            $this.attr('per', Math.floor(this.animatedValue) + '%');
-        },
-        complete: function(){
-            $this.attr('per', Math.floor(this.animatedValue) + '%');
-        }
-    });
-});
+
+    const navTogglerBtn=document.querySelector(".nav-toggler"),
+        aside=document.querySelector(".aside");
+
+    navTogglerBtn.addEventListener("click",() =>{
+        asideSectionTogglerBtn();
+    })
+function asideSectionTogglerBtn(){
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+}  
